b236ff0e2f07c483216015e75e0db673d23d3d0b
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-07-19 15:53:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+status = "";
+object = [];
+alarm = "alarm.mp3";
+
+function setup() {
+    canvas = createCanvas(640, 460);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    objectDetector = ml5.objectDetector("cocossd", modelloaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+
+function modelloaded() {
+    console.log("Model Loaded");
+    status = true;
+}
+
+function getResult(error, result) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(result);
+        object = result;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 640, 460);
+    r = random(255);
+    g = random(255);
+    b = random(255);
+    if (status != "") {
+        objectDetector.detect(video, getResult);
+        for (i = 0; i < object.length; i++) {
+            strokeWeight(1);
+            fill(r, g, b);
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 22);
+            textSize(25);
+            noFill();
+            stroke(r, g, b);
+            strokeWeight(3);
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+            document.getElementById("status").innerHTML = "Object Detected";
+            if (object[i].label == "person") {
+                document.getElementById("babysts").innerHTML = "Baby Found";
+            } else if(object[i].label != "person" || object[i].label == "") {
+                document.getElementById("babysts").innerHTML = "Baby Not Found";
+                alarm.play();
+            }
+        }
+    }
+}
+
+function stop() {
+    alarm.stop();
+}
